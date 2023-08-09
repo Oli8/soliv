1abcb5c382dcbe7cfa653d1c5533dad90d4143b1
Olivier <olivier.crete@epitech.eu>
2023-08-09 11:40:35 +0200

    Timelock evol (#6)
    * update after timelockgroups
    
    * use custom error
    
    * UserLockTimeChanged event
    
    * extends tests

--- contracts/access/TimeLock.sol
+++ contracts/access/TimeLock.sol
@@ -1,15 +1,23 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.18;
 
-abstract contract TimeLock {
+import {TimeLockCommon} from "./TimeLockCommon.sol";
+
+abstract contract TimeLock is TimeLockCommon {
     uint256 private _timeLockDuration;
 
-    mapping(address => uint256) private _timeLocks;
+    mapping(address user => uint256 timestamp) private _timeLocks;
+
+    error LockedUser(address user);
 
     event DurationChanged(
         uint256 previousDuration,
         uint256 newDuration
+    );
+    event UserLockTimeChanged(
+        address indexed user,
+        uint256 timestamp
     );
 
     constructor(uint256 timeLockDuration) {
@@ -18,10 +26,9 @@ abstract contract TimeLock {
 
     modifier timeLocked() {
         address caller = msg.sender;
-        require(
-            !isLocked(caller),
-            "TimeLock: Account under timelock"
-        );
+        if (isLocked(caller)) {
+            revert LockedUser(caller);
+        }
         _lock(caller);
         _;
     }
@@ -31,7 +38,11 @@ abstract contract TimeLock {
     }
 
     function releaseTime(address user) public view returns (uint256) {
-        uint256 userLockTime = _timeLocks[user];
+        return _timeLocks[user];
+    }
+
+    function lockTimeRemaining(address user) public view returns (uint256) {
+        uint256 userLockTime = releaseTime(user);
         uint256 timeNow = _now();
         if (timeNow >= userLockTime) {
             return 0;
@@ -41,24 +52,26 @@ abstract contract TimeLock {
     }
 
     function isLocked(address user) public view returns (bool) {
-        return _now() <= _timeLocks[user];
+        return _now() < releaseTime(user);
     }
 
     function _lock(address user) internal {
-        _timeLocks[user] = _now() + _timeLockDuration;
+        _setUserLockTime(user, _now() + _timeLockDuration);
     }
 
     function _setDuration(uint256 newDuration) internal {
         uint256 previousDuration = _timeLockDuration;
         _timeLockDuration = newDuration;
+
         emit DurationChanged(previousDuration, newDuration);
     }
 
     function _clear(address user) internal {
-        _timeLocks[user] = 0;
+        _setUserLockTime(user, 0);
     }
 
-    function _now() private view returns (uint256) {
-        return block.timestamp;
+    function _setUserLockTime(address user, uint256 timestamp) internal {
+        _timeLocks[user] = timestamp;
+        emit UserLockTimeChanged(user, timestamp);
     }
 }
